d34748c6d3049cbddad90c7ac1c66d743fb06272
Luis Ocasio <ocasio.dev@gmail.com>
2021-02-17 18:46:46 +0000

    config env

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       resolve: `gatsby-source-wordpress`,
       options: {
         // the only required plugin option for WordPress is the GraphQL url.
-        url: process.env.WPGRAPHQL_URL || `http://34.237.1.113/graphql`,
+        url: process.env.WPGRAPHQL_URL,
       },
     },
     `gatsby-transformer-sharp`,
