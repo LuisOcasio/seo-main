a5677b100155643fe161dad07828ad9cc77660c4
Luis Ocasio <ocasio.dev@gmail.com>
2021-02-17 20:55:31 +0000

    correct url

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       resolve: `gatsby-source-wordpress`,
       options: {
         // the only required plugin option for WordPress is the GraphQL url.
-        url: process.env.WPGRAPHQL_URL,
+        url: `http://34.237.1.113/graphql`,
       },
     },
     `gatsby-transformer-sharp`,
